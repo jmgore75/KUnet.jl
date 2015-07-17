0b60194ce07be32d7567b9b2e9e8922e5e8011d9
Jeremy Gore <jmgore75@gmail.com>
2015-07-17 07:41:33 -0400

    Use no .so extensions to support OS X
    Also needed to remove .so extension from ccall.

--- src/gpu.jl
+++ src/gpu.jl
@@ -34,7 +34,7 @@ const libkunet = find_library(["libkunet"], [Pkg.dir("KUnet/src")])
 function gpumem()
     mfree=Csize_t[1]
     mtotal=Csize_t[1]
-    ccall((:cudaMemGetInfo,"libcudart.so"),Cint,(Ptr{Csize_t},Ptr{Csize_t}),mfree,mtotal)
+    ccall((:cudaMemGetInfo,"libcudart"),Cint,(Ptr{Csize_t},Ptr{Csize_t}),mfree,mtotal)
     convert(Int,mfree[1])
 end
 
